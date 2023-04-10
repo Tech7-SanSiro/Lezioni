68f7dda176d5046236944efe39b05f2f45c3e8b2
Giuseppe Cannizzaro <giucannizzaro@gmail.com>
2023-04-10 22:28:02 +0200

    Added JS and CSS

---
+++ 00 - Starting concepts/script.js
@@ -0,0 +1,23 @@
+const submitButton = document.querySelector("input[type='submit']");
+const tableBody = document.querySelector("tbody");
+
+submitButton.addEventListener("click", function(event) {
+  event.preventDefault(); // Previeni l'invio del form
+
+  const nomeInput = document.getElementById("nome");
+  const emailInput = document.getElementById("email");
+
+  // Crea la nuova riga della tabella
+  const row = document.createElement("tr");
+  const nomeCell = document.createElement("td");
+  nomeCell.textContent = nomeInput.value;
+  const emailCell = document.createElement("td");
+  emailCell.textContent = emailInput.value;
+  row.appendChild(nomeCell);
+  row.appendChild(emailCell);
+  tableBody.appendChild(row);
+
+  // Resetta i campi del form
+  nomeInput.value = "";
+  emailInput.value = "";
+});
